a5f57101ed0d11dc136ad440f21442acec6751fa
Cintia Santos <37563871+CintiaSantos@users.noreply.github.com>
2022-04-27 19:49:23 -0400

    created routes

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/about', (req, res) => {
+  res.send('Hello from about!')
+})
+
+app.post('/about', (req, res) => {
+  res.send('Hello from post about!')
+})
+
+app.put('/marshmellows', (req, res) => {
+  res.send('I actually love them')
+})
+
+app.delete('/marshmellows', (req, res) => {
+  res.send('No I don\'t')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
