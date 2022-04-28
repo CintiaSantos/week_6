c9940726e70936b761bd0549acf3b752c12fd119
Cintia Santos <37563871+CintiaSantos@users.noreply.github.com>
2022-04-27 23:48:06 -0400

    working routes

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-  res.send('Hello from about!')
+  res.send('Hello from the other side!')
 })
 
+app.get('/contact', (req, res) => {
+  res.send('Send me a message')
+})
 app.post('/about', (req, res) => {
-  res.send('Hello from post about!')
+  res.send('Hello but I will probably not answer!')
 })
 
 app.put('/marshmellows', (req, res) => {
@@ -22,6 +25,23 @@ app.delete('/marshmellows', (req, res) => {
   res.send('No I don\'t')
 })
 
+app.head('/marshmellows', (req, res) => {
+  res.send('I like marshmellows on my sweetpotatoes')
+})
+
+app.put('/bananas', (req, res) => {
+  res.send('Great for knee pain ')
+})
+
+app.patch('/bananas', (req, res) => {
+  res.send('I do not know what patch does?? Anyone?')
+})
+
+app.link('/pandulce', (req, res) => {
+  res.send('Pan dulce Doña Mary and some coffee, I will always want that')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
